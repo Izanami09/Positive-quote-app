d24513b3c46b68da346e596302e1e60aefd26c07
Rohan <078bct070@ioepc.edu.np>
2023-10-19 16:23:24 +0545

    App is ready

--- app/src/main/java/com/example/positivequotes/Affirmations.kt
+++ app/src/main/java/com/example/positivequotes/Affirmations.kt
@@ -5,9 +5,15 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Card
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
@@ -18,12 +24,24 @@ import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import com.example.positivequotes.data.DataSource
 import com.example.positivequotes.model.Affirmation
+import com.example.positivequotes.ui.theme.md_theme_dark_onSurfaceVariant
 
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Preview
 @Composable
 fun AffirmationApp(){
-    AffirmationCard(affirmation = Affirmation(R.string.affirmation1, R.drawable.image1))
+    Scaffold(
+        topBar = {
+            TopAppBar(title = { Text(text = stringResource(id = R.string.app_name), style = MaterialTheme.typography.titleMedium)},
+                colors = TopAppBarDefaults.mediumTopAppBarColors(
+                    containerColor = md_theme_dark_onSurfaceVariant
+                ))
+        }
+    ) {
+        it -> AffirmationList(affirmationList = DataSource().loadAffirmations(), modifier = Modifier.padding(it))
+    }
+
 }
 
 
@@ -52,5 +70,12 @@ fun AffirmationCard(affirmation: Affirmation, modifier: Modifier = Modifier) {
 
 @Composable
 fun AffirmationList(affirmationList: List<Affirmation>, modifier:Modifier = Modifier) {
-    
+    LazyColumn(modifier = modifier) {
+        items(affirmationList){
+            singleCardAffirmation -> AffirmationCard(
+                affirmation = singleCardAffirmation,
+                modifier = Modifier.padding(8.dp)
+            )
+        }
+    }
 }
